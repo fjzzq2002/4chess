3a86f83581c56df8ab45f46b54eafdf9347abbc1
fjzzq2002 <fjzzq2002@163.com>
2017-06-03 17:03:38 +0800

    bug fix?

--- ai.cpp
+++ ai.cpp
@@ -34,7 +34,7 @@ typedef vector<int> vi;
 #define SZ 666666
 struct sta
 {
-int p,m[11][11],c[11],s;
+int p,m[12][12],c[12],s;
 sta() {memset(m,0,sizeof m);memset(c,0,sizeof c);}
 int jud()
 {
@@ -111,7 +111,7 @@ int jc(int cur,ld K)
 		if(chh=ch[cur][k])
 			t+=did[chh]; ++t;
 	}
-	int a;
+	int a=1;
 	ld maxn=-1e18;
 	for(int k=1;k<=7;k++)
 	{
@@ -128,21 +128,23 @@ int ww,pss=0; ld wss=0;
 void try_play()
 {
 	static int ps[SZ]; int sn=0;
-	int cur=1;
+	int cur=1,tat=0;
 	while(!ss[cur].end())
 	{
+		++tat;
 		int s=jc(cur,C),nx,chh;
 		if(chh=ch[cur][s]) nx=chh;
 		else ch[cur][s]=nx=alc(ss[cur].go(s));
 		cur=nx, ps[++sn]=cur;
 	}
 	int x=ss[cur].jud();
-	ld g=x?(x==ww):0;
+	ld g=x?(x==ww):0.5;
 	wss+=g; ++pss;
 	for(int i=1;i<=sn;i++)
 		++did[ps[i]],win[ps[i]]+=g;
 }
 sta g;
+char TMP[23333];
 int main()
 {
 	for(int i=7;i>=1;--i)
@@ -152,13 +154,11 @@ int main()
 	}
 	for(int i=1;i<=7;i++)
 		while(g.m[i][g.c[i]+1]) ++g.c[i];
-	cin>>ww; g.p=ww; g.s=5; alc(g); int t=clock();
+	cin>>ww; g.p=ww; g.s=14; alc(g); int t=clock();
 	while((clock()-t)<2*CLOCKS_PER_SEC) try_play();
 	ld cb=wss/pss*100;
-	static char ss[23333];
-	sprintf(ss,"title AI%d: %.3f%% win prob  out of %d matches",ww,cb,pss);
-	system(ss); int l=jc(1,0);
+	sprintf(TMP,"title AI%d: %.3f%% win prob  out of %d matches",ww,cb,pss);
+	system(TMP); int l=jc(1,0);
 	printf("%d\n",l);
 }
-
 
